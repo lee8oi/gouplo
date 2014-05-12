d04ef200c3b93b48935d17178c6788725f49c31c
Lee Forest <lee8oi@gmail.com>
2014-05-11 23:03:42 -0400

    copied readme doc into package doc comment

--- main.go
+++ main.go
@@ -5,8 +5,15 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 /*
-Gouplo is a simple & easy-to-use fileserver, written in Go (golang.org),
-that features a basic login system and a multiple-file upload form.
+
+Gouplo is a simple & easy-to-use fileserver, written in Go (golang.org), that features a basic login system and a multiple-file upload form.
+
+**features**
+* jQuery/Ajax for dynamic form submissions without page reload.
+* HTTP Digest Authentication for basic login capability (better than no login).
+* Multiple file handling for batch upload jobs (no progress bar).
+* Easily configurable with commandline flags (run 'gouplo -help' for more information).
+
 */
 package main
 
