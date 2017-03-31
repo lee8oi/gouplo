c9e911072f80e18ec1d3f3d438e1ab08f8338b18
Lee Forest <lee8oi@gmail.com>
2017-03-30 23:43:01 -0400

    Switched to basic auth, TLS, and json config file.

--- main.go
+++ main.go
@@ -4,41 +4,43 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-/*
-
-Gouplo is a simple & easy-to-use fileserver, written in Go (golang.org), that features a basic login system and a multiple-file upload form.
-
-**features**
-* jQuery/Ajax for dynamic form submissions without page reload.
-* HTTP Digest Authentication for basic login capability (better than no login).
-* Multiple file handling for batch upload jobs (no progress bar).
-* Easily configurable with commandline flags (run 'gouplo -help' for more information).
-
-*/
+// Gouplo is a simple & easy-to-use fileserver written in Go (golang.org).
 package main
 
 import (
-	"crypto/md5"
+	"encoding/json"
 	"flag"
-	"fmt"
-	auth "github.com/abbot/go-http-auth"
 	"html/template"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
-var homefile = flag.String("home", "home.html", "full path to home html template file")
-var uploadDir = flag.String("upload", "upload/", "full path to upload directory")
-var publicDir = flag.String("public", "public/", "full path to public directory")
-var createDirs = flag.Bool("create", false, "create missing directories")
-var username = flag.String("user", "myuser", "set server login user name")
-var password = flag.String("pass", "mypass", "set server login password")
-var realm = flag.String("realm", "myrealm", "set server realm")
+var cfgPath = flag.String("config", "config.json", "path to config file (in JSON format)")
+var cfg config
 
-func homeHandler(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
-	template.Must(template.ParseFiles(*homefile)).Execute(w, r.Host)
+func main() {
+	flag.Parse()
+	cfg = loadConfig(*cfgPath)
+	http.HandleFunc("/", authHandler(indexHandler, cfg.User, cfg.Pass, cfg.Realm))
+	http.HandleFunc("/upload", uploadHandler)
+	http.Handle("/pub/", http.StripPrefix("/pub/", http.FileServer(http.Dir(cfg.PubDir))))
+	go func() {
+		err := http.ListenAndServeTLS(":"+cfg.HTTPSPort, cfg.CertPem, cfg.KeyPem, nil)
+		if err != nil {
+			log.Fatal("ListenAndServeTLS:", err)
+		}
+	}()
+	err := http.ListenAndServe(":"+cfg.HTTPPort, http.RedirectHandler("https://"+cfg.Domain+":"+cfg.HTTPSPort, 301))
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	template.Must(template.ParseFiles(cfg.IndexFile)).Execute(w, r.Host)
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,14 +54,14 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		m := r.MultipartForm
 		files := m.File["files"]
-		for i, _ := range files {
+		for i := range files {
 			file, err := files[i].Open() //open file
 			defer file.Close()
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			dst, err := os.Create(*uploadDir + files[i].Filename) //ensure destination is writeable
+			dst, err := os.Create(cfg.UpDir + "/" + files[i].Filename) //ensure destination is writeable
 			defer dst.Close()
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -74,36 +76,35 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func CalculateHA1(user, rlm, pass string) string {
-	h := md5.New()
-	fmt.Fprintf(h, "%s:%s:%s", user, rlm, pass)
-	ha1 := h.Sum(nil)
-	return fmt.Sprintf("%x", ha1)
+// config type contains the necessary server configuration strings.
+type config struct {
+	HTTPPort, HTTPSPort, IndexFile, PubDir, UpDir, User, Pass, Realm,
+	Domain, CertPem, KeyPem string
 }
 
-func Secret(user, rlm string) string {
-	if user == *username {
-		return CalculateHA1(user, rlm, *password)
+// loadConfig loads configuration values from file.
+func loadConfig(path string) (c config) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return ""
-}
-
-func init() {
-	flag.Parse()
-	if *createDirs == true {
-		if err := os.Mkdir(*uploadDir, 0777); err == nil {
-			fmt.Println("directory created: ", *uploadDir)
-		}
-		if err := os.Mkdir(*publicDir, 0777); err == nil {
-			fmt.Println("directory created: ", *publicDir)
-		}
+	err = json.Unmarshal(b, &c)
+	if err != nil {
+		log.Fatal(err)
 	}
+	return
 }
 
-func main() {
-	authenticator := auth.NewDigestAuthenticator(*realm, Secret)
-	http.HandleFunc("/", authenticator.Wrap(homeHandler))
-	http.HandleFunc("/upload", uploadHandler)
-	http.Handle("/pub/", http.StripPrefix("/pub/", http.FileServer(http.Dir(*publicDir))))
-	http.ListenAndServe(*addr, nil)
+// authHandler wraps a handler function to provide http basic authentication.
+func authHandler(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, p, ok := r.BasicAuth()
+		if !ok || u != username || p != password {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("401 Unauthorized\n"))
+			return
+		}
+		handler(w, r)
+	}
 }
